6e291dce6f44d0b27478d17953d352e5b1975bb6
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2015-11-16 09:24:35 +0900

    ボタンの追加

--- app/src/main/java/com/android_lesson/aomori/hello2/MainActivity.java
+++ app/src/main/java/com/android_lesson/aomori/hello2/MainActivity.java
@@ -1,13 +1,17 @@
 package com.android_lesson.aomori.hello2;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        Button btn = new Button(getApplicationContext());
+        btn.setText("HELLO");
+        setContentView(btn);
     }
 }
